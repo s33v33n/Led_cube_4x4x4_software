6fb59a4b6615289db7d7e9f86e95ed7056e84e20
michal c <michalekczer@gmail.com>
2025-10-03 13:35:28 +0200

    PWM - timers from main.c (src) added to timers folder

---
+++ src_timer/timers_setup.h
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void timer0_pwm_on(void);
+void timer0_ISR_effect4_on(void);
+void timer0_pwm_off(void);
+
+void timer2_pwm_on(void);
+void timer2_ISR_effect5_on(void);
+void timer2_pwm_off(void);
+
+
+// void timer1_setup(void);
+// void timer1_turn_on_ISR(void);
+// void timer1_turn_off_ISR(void);
+
